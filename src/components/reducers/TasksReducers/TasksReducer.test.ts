bd0bb548046575f8a7d905eafd535689f0d81c32
Alonesa <alenakrasnov@mail.ru>
2023-08-12 15:35:28 +0100

    added Remove task action for task reducer and test for it

--- src/components/reducers/TasksReducers/TasksReducer.test.ts
+++ src/components/reducers/TasksReducers/TasksReducer.test.ts
@@ -1,40 +1,61 @@
 import {v1} from 'uuid';
 import {TasksType} from '../../../Todolist';
-import {AddTaskAC, TasksReducer} from './TasksReducer';
+import {AddTaskAC, RemoveTaskAC, TasksReducer} from './TasksReducer';
 
 let todolistID1: string;
 let todolistID2: string;
 let startState: TasksType
 
-beforeEach(()=> {
+beforeEach(() => {
   todolistID1 = v1();
   todolistID2 = v1();
   
   startState = {
 	[todolistID1]: [
-	{id: v1(), title: 'HTML&CSS', isDone: true},
-	{id: v1(), title: 'JS', isDone: true},
-	{id: v1(), title: 'ReactJS', isDone: false},
-	{id: v1(), title: 'Rest API', isDone: false},
-	{id: v1(), title: 'GraphQL', isDone: false},
-  ],
+	  {id: '1', title: 'HTML&CSS', isDone: true},
+	  {id: '2', title: 'JS', isDone: true},
+	  {id: '3', title: 'ReactJS', isDone: false},
+	  {id: '4', title: 'Rest API', isDone: false},
+	  {id: '5', title: 'GraphQL', isDone: false},
+	],
 	[todolistID2]: [
-	{id: v1(), title: 'HTML&CSS2', isDone: true},
-	{id: v1(), title: 'JS2', isDone: true},
-	{id: v1(), title: 'ReactJS2', isDone: false},
-	{id: v1(), title: 'Rest API2', isDone: false},
-	{id: v1(), title: 'GraphQL2', isDone: false},
-  ]
+	  {id: '1', title: 'HTML&CSS2', isDone: true},
+	  {id: '2', title: 'JS2', isDone: true},
+	  {id: '3', title: 'ReactJS2', isDone: false},
+	  {id: '4', title: 'Rest API2', isDone: false},
+	  {id: '5', title: 'GraphQL2', isDone: false},
+	],
   }
 })
 
-test("New task should be added to the correct todolist", ()=> {
-  let title = 'New task title';
-  
-  const endState = TasksReducer(startState, AddTaskAC(todolistID2,title))
-  
-  expect(startState[todolistID2].length).toBe(5);
-  expect(endState[todolistID2].length).toBe(6);
-  expect(endState[todolistID2][0].title).toBe(title);
+test('New task should be added to the correct todolist', () => {
+	let title = 'New task title';
+	
+	const endState = TasksReducer(startState, AddTaskAC(todolistID2, title))
+	
+	expect(startState[todolistID2].length).toBe(5);
+	expect(endState[todolistID2].length).toBe(6);
+	expect(endState[todolistID2][0].title).toBe(title);
   }
 )
+
+test('Task should be removed from correct todolist', () => {
+  const endState = TasksReducer(startState, RemoveTaskAC(todolistID1, '1'))
+  
+  expect(endState).toEqual(
+	{
+	  [todolistID1]: [
+		{id: '2', title: 'JS', isDone: true},
+		{id: '3', title: 'ReactJS', isDone: false},
+		{id: '4', title: 'Rest API', isDone: false},
+		{id: '5', title: 'GraphQL', isDone: false},
+	  ],
+	  [todolistID2]: [
+		{id: '1', title: 'HTML&CSS2', isDone: true},
+		{id: '2', title: 'JS2', isDone: true},
+		{id: '3', title: 'ReactJS2', isDone: false},
+		{id: '4', title: 'Rest API2', isDone: false},
+		{id: '5', title: 'GraphQL2', isDone: false},
+	  ],
+	})
+})
--- src/components/reducers/TasksReducers/TasksReducer.tsx
+++ src/components/reducers/TasksReducers/TasksReducer.tsx
@@ -13,21 +13,34 @@ export const TasksReducer = (state: TasksType, action: ActionsType) => {
         }, ...state[action.payload.tlId]]
       }
     }
+    case 'REMOVE-TASK': {
+      return {...state, [action.payload.tlId]: [...state[action.payload.tlId].filter(tl=> tl.id !== action.payload.id)]}
+    }
     default:
       return state
   }
   
 }
 
-type ActionsType = AddTaskACType
+type ActionsType = AddTaskACType | RemoveTaskACType
 
 type AddTaskACType = ReturnType<typeof AddTaskAC>
+type RemoveTaskACType = ReturnType<typeof RemoveTaskAC>
 
 export const AddTaskAC = (tlId: string, title: string) => {
   return {
     type: 'ADD-TASK',
     payload: {
       tlId, title
+    }
+  } as const
+}
+
+export const RemoveTaskAC = (tlId: string, id: string) => {
+  return {
+    type: 'REMOVE-TASK',
+    payload: {
+      tlId, id
     }
   } as const
 }
