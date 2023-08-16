194b3f6a3b9f7c46a9d80d42f840bbf3a8edafb1
Alonesa <alenakrasnov@mail.ru>
2023-08-16 23:00:21 +0100

    added init state mock data

--- src/state/reducers/TodolistsReducer/TodolistsReducer.tsx
+++ src/state/reducers/TodolistsReducer/TodolistsReducer.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {FilterValueType, TodolistsType} from '../../../Todolist';
 import {v1} from 'uuid';
 
-const initialState:Array<TodolistsType> = []
+const initialState:Array<TodolistsType> = [
+  {id: 'todolistID1', title: 'What to learn', filter: 'All'},
+  {id: 'todolistID2', title: 'What to buy', filter: 'All'},
+]
 
 export const TodolistsReducer = (state: Array<TodolistsType> = initialState, action: ActionTypes): Array<TodolistsType> => {
   switch (action.type) {
