6bdf3badc3665609170488be33f0443f6e321d22
Alonesa <alenakrasnov@mail.ru>
2023-08-30 21:21:54 +0100

    remove unwanted comments

--- src/stories/TaskWithRedux.stories.tsx
+++ src/stories/TaskWithRedux.stories.tsx
@@ -3,29 +3,28 @@ import {TaskWithRedux} from '../components/Task/TaskWithRedux';
 import {TaskStoreProviderDecorator} from './decorators/TaskStoreProviderDecorator';
 import {store} from '../state/store';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof TaskWithRedux> = {
   title: 'Task/TaskWithRedux',
   component: TaskWithRedux,
+  parameters: {
+	layout: 'centered'
+  },
   tags: ['autodocs'],
   decorators: [TaskStoreProviderDecorator],
   args: {
-    task: {id: '01', title: 'Task for storybook', isDone: false},
-    tlId: store.getState().todolists[1].id
-  }
+	task: {id: '01', title: 'Task for storybook', isDone: false},
+	tlId: store.getState().todolists[1].id
+  },
 };
 
-
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 
 export default meta;
 type Story = StoryObj<typeof TaskWithRedux>;
 
-export const TaskWithReduxUnchecked: Story={
-}
+export const TaskWithReduxUnchecked: Story = {}
 
-export const TaskWithReduxChecked: Story={
+export const TaskWithReduxChecked: Story = {
   args: {
-    task: {id: '01', title: 'Task for storybook', isDone: true},
+	task: {id: '01', title: 'Task for storybook', isDone: true},
   }
 }
