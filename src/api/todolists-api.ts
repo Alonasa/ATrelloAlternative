2df0e2c059398060992b0528a4121f9b9ffd28f6
Alonesa <alenakrasnov@mail.ru>
2023-09-08 16:53:56 +0100

    fix types

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -7,15 +7,11 @@ export type TodolistType = {
   title: string
 }
 
-type CreateTodolistResponseType = {
+export type ResponseType<T ={}> = {
   resultCode: number
   messages: Array<string>
-  fieldsErrors: Array<string>
-  data: {
-	item: TodolistType
-  }
+  data: T
 }
-
 
 export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
@@ -25,25 +21,18 @@ export const instance = axios.create({
   }
 })
 
-type UpdateTodolistResponseType = {
-  resultCode: number
-  messages: Array<string>
-  fieldsErrors: Array<string>
-  data: {}
-}
-
 
 export const TodolistsAPI = {
   getTodolists() {
 	return instance.get<Array<TodolistType>>('todo-lists')
   },
   createTodolist() {
-	return instance.post<CreateTodolistResponseType>('todo-lists', {title: 'React'})
+	return instance.post<ResponseType<{item: TodolistType}>>('todo-lists', {title: 'React'})
   },
   updateTodolist(todolistId: string, title: string) {
-	return instance.put(`todo-lists/${todolistId}`, {title})
+	return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
   },
   deleteTodolist(todolistId: string) {
-	return instance.delete(`todo-lists/${todolistId}`)
+	return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
   }
 }
--- src/stories/Todolists-api.stories.tsx
+++ src/stories/Todolists-api.stories.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {TodolistsType} from '../Todolist1';
-import {TodolistsAPI, TodolistType} from '../api/todolists-api';
+import {ResponseType, TodolistsAPI, TodolistType} from '../api/todolists-api';
 
 export default {
   title: 'API'
@@ -20,21 +19,21 @@ export const GetTodolilsts = () => {
 }
 
 export const CreateTodolist = () => {
-  const [state, setState] = useState<any | null>(null)
+  const [state, setState] = useState<TodolistType | null>(null)
   useEffect(() => {
 	TodolistsAPI.createTodolist()
 	  .then((res) => {
-		setState(res.data)
+		setState(res.data.data.item)
 	  })
   }, [])
   return <div>{JSON.stringify(state)}</div>
 }
 
 export const UpdateTodolist = () => {
-  const [state, setState] = useState<TodolistsType | null>(null)
+  const [state, setState] = useState<ResponseType | null>(null)
   
   useEffect(() => {
-	TodolistsAPI.updateTodolist('755fb806-9456-4158-a8a7-3c2f8ac82d80', 'React11')
+	TodolistsAPI.updateTodolist('755fb806-9456-4158-a8a7-3c2f8ac82d80', 'React5611')
 	  .then((res) => {
 		setState(res.data)
 	  })
@@ -43,7 +42,7 @@ export const UpdateTodolist = () => {
 }
 
 export const DeleteTodolist = () => {
-  const [state, setState] = useState<TodolistsType | null>(null)
+  const [state, setState] = useState<ResponseType | null>(null)
   
   useEffect(() => {
 	TodolistsAPI.deleteTodolist('8457de01-405e-4e05-97a0-2c6960faac32')
