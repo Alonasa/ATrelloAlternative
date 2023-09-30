b5d2d6a0334b69a935e8cdb1bd365f6e0957ba62
Alonesa <alenakrasnov@mail.ru>
2023-09-30 22:12:18 +0100

    added error handling for add task

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,24 @@
+import {
+  setAppErrorAC,
+  SetAppErrorACType,
+  setAppStatusAC, SetAppStatusACType
+} from '../app/app-reducer';
+import {Dispatch} from 'redux';
+import {ResponseType} from '../api/todolists-api';
+
+export const handleServerNetworkError = (dispatch: Dispatch<ErrorUtilsDispatchType>, error: string) => {
+	dispatch(setAppStatusAC('failed'))
+	dispatch(setAppErrorAC(error))
+}
+
+type ErrorUtilsDispatchType = SetAppErrorACType | SetAppStatusACType
+
+export const handleServerAppError = <T>(dispatch: Dispatch<ErrorUtilsDispatchType>, data: ResponseType<T>) => {
+  const error = data.messages[0]
+  if (error) {
+	dispatch(setAppErrorAC(error))
+  } else {
+	dispatch(setAppStatusAC('failed'))
+  }
+  dispatch(setAppStatusAC('failed'))
+}
