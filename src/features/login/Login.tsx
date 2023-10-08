fc3637a53152ca5ac50a8b3fe73cf08a856a8e79
Alonesa <alenakrasnov@mail.ru>
2023-10-08 22:04:04 +0100

    added simple validation for a fields email, pass, remember

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -9,6 +9,11 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from 'formik';
 
+type  FormikErrorType = {
+  email?: string,
+  password?: string
+}
+
 export const Login = () => {
   const formik = useFormik({
 	initialValues: {
@@ -16,11 +21,22 @@ export const Login = () => {
 	  password: '',
 	  rememberMe: false
 	},
+	validate: (values) => {
+	  const errors: FormikErrorType = {}
+	  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+	  if (!values.email) {
+		errors.email = 'Required';
+	  } else if (!regex.test(values.email)) {
+		errors.email = 'Invalid email address'
+	  }
+	  return errors
+	},
 	onSubmit: values => {
 	  alert(JSON.stringify(values, null, 2))
 	},
   });
-  console.log('values: ', formik.values)
+  const res = Object.values(formik.errors)
+  
   return <Grid container justifyContent={'center'}>
 	<Grid item justifyContent={'center'}>
 	  <FormControl>
@@ -34,25 +50,29 @@ export const Login = () => {
 		  <p>Email: free@samuraijs.com</p>
 		  <p>Password: free</p>
 		</FormLabel>
-		<FormGroup>
-		  <TextField label="Email"
-					 margin="normal"
-					 name={'email'}
-					 onChange={formik.handleChange}
-					 value={formik.values.email}/>
-		  <TextField type="password" label="Password"
-					 margin="normal"
-					 name={'password'}
-					 onChange={formik.handleChange}
-					 value={formik.values.password}
-		  />
-		  <FormControlLabel label={'Remember me'}
-							control={<Checkbox name={'rememberMe'} onChange={formik.handleChange}
-											   value={formik.values.rememberMe}/>}/>
-		  <Button type={'submit'} variant={'contained'} color={'primary'}>
-			Login
-		  </Button>
-		</FormGroup>
+		<form onSubmit={formik.handleSubmit}>
+		  <FormGroup>
+			<TextField label="Email"
+					   margin="normal"
+					   name={'email'}
+					   onChange={formik.handleChange}
+					   value={formik.values.email}/>
+			{formik.errors.email && <span style={{color: '#965bef'}}>{formik.errors.email}</span>}
+			<TextField type="password" label="Password"
+					   margin="normal"
+					   name={'password'}
+					   onChange={formik.handleChange}
+					   value={formik.values.password}
+			/>
+			<FormControlLabel label={'Remember me'}
+							  control={<Checkbox name={'rememberMe'}
+												 onChange={formik.handleChange}
+												 value={formik.values.rememberMe}/>}/>
+			<Button type={'submit'} variant={'contained'} color={'primary'} disabled={!!res.length}>
+			  Login
+			</Button>
+		  </FormGroup>
+		</form>
 	  </FormControl>
 	</Grid>
   </Grid>
