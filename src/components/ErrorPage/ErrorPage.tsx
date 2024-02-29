41ece7ea239ddcd5bfd020e439a5f4ce01bb260a
Alonesa <alenakrasnov@mail.ru>
2024-02-29 23:15:58 +0000

    added information to error page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import {Link} from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
     <Grid item>
       <h1>Page is not found</h1>
+      <p>Try to login <Link to={'/login'}>Login</Link></Link></p>
     </Grid>
   );
 };
