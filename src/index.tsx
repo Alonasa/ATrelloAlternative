3d4fd671c51160ff4b74a66b5c3772f06fd97bcf
Alonesa <alenakrasnov@mail.ru>
2024-03-01 11:17:59 +0000

    changed routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
-import React from "react"; //We are starting
-// to using react, without that import
-// our application will not work as we expected
+import React from "react";
 import ReactDOM from "react-dom/client"; //Framework for
 // building web applications
 import "./index.css";
 import App from "./app/App";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import { BrowserRouter } from "react-router-dom"; // import styles
+import { store } from 'app/store';
+import { HashRouter as Router } from 'react-router-dom';
 
-// React DOM will render App element inside element with id root
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
 );
 
